3d60ae7dfbbfb3b42f8cf86158431d46fad53127
CatherineLucky <62281601+CatherineLucky@users.noreply.github.com>
2020-03-18 11:04:45 +0800

    Add files via upload

--- sql题目答案.sql
+++ sql题目答案.sql
@@ -1,8 +1,9 @@
-select user_id, scene,sessionid,ds from 
+#答案
+select user_id, scene, sessionid, ds from 
 (select  * from user_visit_log 
-group by appid 
-order by count(user_id) desc 
-limit 10) as a
-group by ds 
-order by visit_time desc 
+group by appid                             #按app进行用户数量统计
+order by count(user_id) desc               #按访问人数降序排列
+limit 10) as a                             #筛选出符合条件的app及用户信息
+group by ds                                #按日期进行访问场景统计
+order by visit_time desc                   #按访问时间降序排列
 limit 1;
